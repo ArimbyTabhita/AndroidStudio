794c5cbb825d700706a998a91b3672167d420383
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-07-31 10:30:05 +0700

    Vidio 10
    Spinner Android Studio

--- Konfersisuhu/app/src/main/java/com/arimby/konfersisuhu/MainActivity.java
+++ Konfersisuhu/app/src/main/java/com/arimby/konfersisuhu/MainActivity.java
@@ -1,6 +1,10 @@
 package com.arimby.konfersisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        load();
+        isiSpinner();
+    }
+    public void load(){
+        spinner = findViewById(R.id.spinner);
+    }
+
+    public  void isiSpinner(){
+        String[] isi = {"Celcius To Reamur", "Celcius To Fahrenheit", "Celcius To Kelvin"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }
+
+    public void btnKonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+
+        System.out.println(pilihan);
     }
 }
